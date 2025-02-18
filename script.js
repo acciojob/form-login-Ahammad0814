500550ed1309ff01c45c414c7cc633bb5f582950
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-18 12:26:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
 function getFormValue() {
   // Prevent the form from actually submitting and refreshing the page
-  // event.preventDefault();
 
   // Retrieve values from the First Name and Last Name fields
-  const fname = document.getElementById('fname').value.trim();
-  const lname = document.getElementById('lname').value.trim();
+  const fname = document.querySelector('[name="fname"]').value.trim();
+  const lname = document.querySelector('[name="lname"]').value.trim();
 
   // Concatenate the First Name and Last Name
   const fullName = `${fname} ${lname}`;
 
   // Display the full name in an alert
   alert(fullName);
-}
 
+}
